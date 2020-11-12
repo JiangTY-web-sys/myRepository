70286343e70990723472579b99cbb0b012aafef5
JiangTY-web-sys <jiangty0328@163.com>
2020-11-12 11:00:40 +0800

    享元模式(flyweight)

---
+++ DesignPatterns/src/com/mashibing/flyweight/TestString.java
@@ -0,0 +1,23 @@
+package src.com.mashibing.flyweight;
+
+
+/**
+ * @description :
+ * @author：jty
+ * @date: 2020-11-12
+ * @sine: 0.0.1
+ */
+public class TestString {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "abc";
+        String s3 = new String("abc");
+        String s4 = new String("abc");
+
+        System.out.println(s1 == s2);
+        System.out.println(s1 == s3);
+        System.out.println(s3 == s4);
+        System.out.println(s3.intern() == s1);
+        System.out.println(s3.intern() == s4.intern());
+    }
+}
